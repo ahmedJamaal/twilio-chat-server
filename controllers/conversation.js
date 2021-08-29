b254965057c9a8f721d73444a731145a8046a668
Zara Cooper <zramcooper@gmail.com>
2021-08-29 15:51:19 +0300

    Add error handling

--- controllers/conversation.js
+++ controllers/conversation.js
@@ -8,19 +8,23 @@ async function StartConversation(req, res, next) {
     const conversationTitle = req.body.conversationTitle;
     const username = req.body.username;
 
-    if (conversationTitle && username) {
-        const conversation = await client.conversations.conversations
-            .create({ friendlyName: conversationTitle });
+    try {
+        if (conversationTitle && username) {
+            const conversation = await client.conversations.conversations
+                .create({ friendlyName: conversationTitle });
 
-        req.session.token = createToken(username, conversation.chatServiceSid);
-        req.session.username = username;
+            req.session.token = createToken(username, conversation.chatServiceSid);
 
-        const participant = await client.conversations.conversations(conversation.sid)
-            .participants.create({ identity: username })
+            const participant = await client.conversations.conversations(conversation.sid)
+                .participants.create({ identity: username })
 
-        res.send({ conversation, participant });
-    } else {
-        next({ message: 'Missing conversation title or username' });
+            res.send({ conversation, participant });
+        } else {
+            next({ message: 'Missing conversation title or username' });
+        }
+    }
+    catch (error) {
+        next({ error, message: 'There was a problem creating your conversation' });
     }
 }
 
@@ -30,19 +34,22 @@ async function AddParticipant(req, res, next) {
     const username = req.body.username;
     const conversationSid = req.params.id;
 
-    const conversation = await client.conversations.conversations
-        .get(conversationSid).fetch();
+    try {
+        const conversation = await client.conversations.conversations
+            .get(conversationSid).fetch();
 
-    if (username && conversationSid) {
-        req.session.token = createToken(username, conversation.chatServiceSid);
-        req.session.username = username;
+        if (username && conversationSid) {
+            req.session.token = createToken(username, conversation.chatServiceSid);
 
-        const participant = await client.conversations.conversations(conversationSid)
-            .participants.create({ identity: username })
+            const participant = await client.conversations.conversations(conversationSid)
+                .participants.create({ identity: username })
 
-        res.send({ conversation, participant });
-    } else {
-        next({ message: 'Missing username or conversation Sid' });
+            res.send({ conversation, participant });
+        } else {
+            next({ message: 'Missing username or conversation Sid' });
+        }
+    } catch (error) {
+        next({ error, message: 'There was a problem adding a participant' });
     }
 }
 
